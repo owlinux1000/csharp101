ffa532c21b16aa8d6b75646fa8b2abd2cdef7fbf
owlinux1000 <encry1024@gmail.com>
2022-07-18 12:02:16 +0900

    Add a sample of how to utilize Win32API from C#

---
+++ win32api/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// [DllImport]するためにはこれが必要っぽい
+using System.Runtime.InteropServices;
+namespace Win32API;
+class Program {
+
+    // 使うDLLを指定する
+    [DllImport("user32.dll")]
+    // 関数定義をC#の文脈で行う
+    static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    static void Main(string[] args) {
+        // nullはダメみたいで、IntPtr.Zeroでないといけないみたい
+        MessageBox(IntPtr.Zero, "MessageBox called from C#", "", 0);
+    }
+}
