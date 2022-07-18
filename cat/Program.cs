ece17b20b5392c7c6963cdaa74bea73ea4c66711
owlinux1000 <encry1024@gmail.com>
2022-07-18 17:26:39 +0900

    Add a sample for tiny UNIX cat

---
+++ cat/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+namespace cat;
+class Program {
+    static void Main(string[] args) {
+
+        // Read from Stdin
+        if(args.Length == 0) {
+            string s = Console.ReadLine();
+            while(s != null) {
+                Console.WriteLine(s);
+                s = Console.ReadLine();
+            }
+        } else {
+            string path = args[0];
+            if(File.Exists(path)) {
+                string s = File.ReadAllText(path);
+                Console.Write(s);
+            } else {
+                Console.WriteLine("Not found");
+            }
+        }
+    }
+}
+
