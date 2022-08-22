2c428f5154a6a09cdabe9217c6ac53e57273fb5d
owlinux1000 <encry1024@gmail.com>
2022-08-22 18:24:04 +0900

    Add Dll and how to use dll in c#

---
+++ fakedll/Fakedll.cs
@@ -0,0 +1,8 @@
+﻿namespace FakeDll {
+    public class FakeDll
+    {
+        public static string hoge() {
+            return "Fakedll's hoge method is called";
+        }
+    }
+}
---
+++ fakedllcaller/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace FakedllCaller{
+    class Program {
+        static void Main() {
+            Console.WriteLine(FakeDll.FakeDll.hoge());
+        }
+    }
+}
